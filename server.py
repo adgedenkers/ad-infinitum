097937538f694e57740db23503c5d6cfe516aaa8
Adge Denkers <adge.denkers@gmail.com>
2022-10-08 09:37:48 -0400

    moved swagger.yml

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import os
 app = connexion.App(__name__, specification_dir='./')
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api('specification/swagger.yml')
+app.add_api('swagger.yml')
 
 @app.route("/")
 def home():
