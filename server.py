7738d79a0683441f2a37a943f0df774d23690908
Adge Denkers <adge.denkers@gmail.com>
2022-10-15 07:53:29 -0400

    started over

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
-from flask import (Flask, render_template)
-import connexion
-import os
+# Import Flask, the Python micro web framework
+from flask import Flask, render_template
+
 
 # Create the application instance
-app = connexion.App(__name__, specification_dir='./')
+app = Flask(__name__, template_folder="templates")
 
-# Read the swagger.yml file to configure the endpoints
-app.add_api('swagger.yml')
 
+# Create a URL route in our application for "/"
 @app.route("/")
 def home():
     """
-    This function responds to the browser URL
+    This function just responds to the browser URL
     localhost:5000/
-    :return:        the rendered template 'home.html'
+
+    :return:        the rendered template "home.html"
     """
     return render_template("home.html")
 
-# if we're running in stand alone mode, run the application
+
 if __name__ == "__main__":
     app.run(debug=True)
